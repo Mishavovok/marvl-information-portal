dcb9cd3c15a6a0a119c1f8692750a04b434fc906
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-01-03 13:39:47 +0300

    The project was created using classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import AppHeader from './components/appHeader/AppHeader';
+import RandomChar from './components/randomChar/RandomChar';
+import CharList from './components/charList/CharList';
+import CharInfo from './components/charInfo/CharInfo';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import decoration from './resourcse/img/vision.png';
+
+class App extends Component {
+  state = {
+    selectedChar: null,
+  };
+
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppHeader />
+        <main>
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
+          <div className="char__content">
+            <ErrorBoundary>
+              <CharList onCharSelected={this.onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={this.state.selectedChar} />
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
