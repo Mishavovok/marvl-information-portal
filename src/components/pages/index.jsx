c1597727f98bf07884d8b66c5793cdbf5ca7f8e4
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-01-30 15:33:58 +0300

    update

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -2,5 +2,6 @@ import MainPage from "./MainPage";
 import ComicsPage from "./ComicsPage";
 import Page404 from "./404";
 import SingleComicPage from "./SingleComicPage";
+import { REPO_NAME } from "./repo";
 
 export {MainPage, ComicsPage, Page404, SingleComicPage, REPO_NAME};
