acb42363feb74141fd28f998b0fac1dc6eb9e993
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-01-30 15:33:08 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AppHeader from './components/appHeader/AppHeader';
-import { MainPage, ComicsPage, Page404, SingleComicPage } from './components/pages';
+import { MainPage, ComicsPage, Page404, SingleComicPage, REPO_NAME } from './components/pages';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={`/${REPO_NAME}/`}>
       <div className="app">
         <AppHeader />
         <main>
---
+++ src/components/pages/repo.jsx
@@ -0,0 +1,1 @@
+export const REPO_NAME = 'marvl-information-portal';
