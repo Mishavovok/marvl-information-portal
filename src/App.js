dcd130f5f591990e30ae24ef0dfad3029ef1b63e
Mikhail Vovk <83818466+Mishavovok@users.noreply.github.com>
2023-02-01 22:12:38 +0300

    optimized page loading made search for heroes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import AppHeader from './components/appHeader/AppHeader';
-import { MainPage, ComicsPage, Page404, SingleComicPage, REPO_NAME } from './components/pages';
+import Spinner from './components/spinner/Spinner';
+import {REPO_NAME} from './components/pages';
 
+import AppHeader from './components/appHeader/AppHeader';
+const Page404 = lazy(() => import('./components/pages/404'));
+const MainPage = lazy(() => import('./components/pages/MainPage'));
+const ComicsPage = lazy(() => import('./components/pages/ComicsPage'));
+const SingleComicLayout = lazy(() => import('./components/pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() =>
+  import('./components/pages/singleCharacterLayout/SingleCharacterLayout'),
+);
+const SinglePage = lazy(() => import('./components/pages/SinglePage'));
 const App = () => {
   return (
-    <Router basename={`/${REPO_NAME}/`}>
+    <Router>
       <div className="app">
         <AppHeader />
         <main>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType="comic"/>} />
+              <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType="character"/>} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
-    </Router> 
+    </Router>
   );
 };
 
--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -3,16 +3,17 @@ import { useState } from 'react';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
+import CharSearchForm from '../charSearchForm/CharSearchForm';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resourcse/img/vision.png';
 
 const MainPage = () => {
-    const [selectedChar, setChar] = useState(null);
+  const [selectedChar, setChar] = useState(null);
 
-    const onCharSelected = (id) => {
-      setChar(id);
-    };
+  const onCharSelected = (id) => {
+    setChar(id);
+  };
 
   return (
     <>
@@ -23,13 +24,18 @@ const MainPage = () => {
         <ErrorBoundary>
           <CharList onCharSelected={onCharSelected} />
         </ErrorBoundary>
-        <ErrorBoundary>
-          <CharInfo charId={selectedChar} />
-        </ErrorBoundary>
+        <div>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharSearchForm />
+          </ErrorBoundary>
+        </div>
       </div>
       <img className="bg-decoration" src={decoration} alt="vision" />
     </>
   );
 };
 
-export default MainPage
+export default MainPage;
